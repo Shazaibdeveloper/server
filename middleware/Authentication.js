901690b4adc4956c94cefb5e807bbd7c14b33630
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-18 15:32:44 +0500

    server dayend

--- middleware/Authentication.js
+++ middleware/Authentication.js
@@ -2,25 +2,28 @@
 const user = require('../models/UserSchema')
   
 
-const Authentication = async (req,res,next) => {
-  try{
-        const token = req.cookies.jwtoken;
-       const validToken = jwt.verify(token,process.env.SECRET_KEY);
-       const rootUser = await user.findOne({_id: validToken._id,"tokens:token": token})
-       if(!rootUser){
-        throw new Error('user not found')
-       }
-
-        req.token = token;
-        req.rootUser = rootUser;
-        req.userID = rootUser._id;
-        next();
-
-
-  }catch(err){
-    res.status(400).send('Unauthorized');
-    console.log(err);
-  }
-}
+const Authentication = async (req, res, next) => {
+    try {
+      const token = req.cookies.jwtoken;
+      if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+  
+      const validToken = jwt.verify(token, process.env.SECRET_KEY);
+      const rootUser = await User.findOne({ _id: validToken._id, tokens: { $elemMatch: { token } } });
+  
+      if (!rootUser) {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+  
+      req.token = token;
+      req.rootUser = rootUser;
+      req.userID = rootUser._id;
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
 
 module.exports = Authentication
