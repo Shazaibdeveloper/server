be53371e097ddc29f492090bb8836860f6f79e60
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-08-30 10:34:57 +0500

    test

--- router/Auth.js
+++ router/Auth.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const cookieParser = require("cookie-parser");
 const jwt = require("jsonwebtoken");
 const bodyParser = require("body-parser"); // Require the body-parser module
 router.use(bodyParser.json()); // Use the JSON parser
 const bycript = require("bcryptjs");
 const auth = require("../middleware/Authentication");
+const cookieParser = require("cookie-parser");
 router.use(cookieParser());
 require("../db/Conn");
 const User = require("../models/UserSchema");
