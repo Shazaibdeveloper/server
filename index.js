c074549db8a990d766b35173c5a3e400e094d4a9
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-11-11 10:44:10 +0500

    complete

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
- const app = express();
+const express = require("express");
+const app = express();
 // const User = require('./models/UserSchema');
-const cors = require('cors')
+const cors = require("cors");
 app.use(express.json());
-app.use(require('./router/Auth'));
-require('./db/Conn')
-const dotenv = require('dotenv');
+app.use(require("./router/Auth"));
+require("./db/Conn");
+const dotenv = require("dotenv");
 
- const corsOptions = {
-    credentials: true,
-    origin: true // Add your frontend origin here (Don't add '/' at the end)
+const corsOptions = {
+  credentials: true,
+  origin: true, // Add your frontend origin here (Don't add '/' at the end)
 };
 app.use("*", cors(corsOptions));
 
-dotenv.config({path:'./config.env'})
- const PORT = process.env.PORT;
+dotenv.config({ path: "./config.env" });
+const PORT = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+}
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on ${PORT}`);
+  console.log(`Server is listening on ${PORT}`);
 });
